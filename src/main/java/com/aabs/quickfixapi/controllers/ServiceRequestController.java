fa895c95a40a72fb60cd4d3e9f1710f2240adcb9
Bineesh <kb.bineesh@gmail.com>
2022-11-29 13:38:01 +0300

    Edit

--- src/main/java/com/aabs/quickfixapi/controllers/ServiceRequestController.java
+++ src/main/java/com/aabs/quickfixapi/controllers/ServiceRequestController.java
@@ -13,7 +13,7 @@ public class ServiceRequestController {
     @GetMapping("/serviceRequests")
     public List<ServiceRequest> Get(){
        List<ServiceRequest> items = new ArrayList<>();
-       items.add(new ServiceRequest(1,"I am a Service Request e fdited"));
+       items.add(new ServiceRequest(1,"I am a Service Request test CI/CD"));
 
        return items;
 
